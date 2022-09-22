125f0e6e20f9dc1fc635ba591c612261c067174c
Eleson Souza <elesonsouza17@hotmail.com>
2022-09-22 08:04:57 -0300

    Layout e style homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { GetStaticProps } from 'next';
 
+import { FiCalendar, FiUser } from 'react-icons/fi';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,13 +25,79 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home(): JSX.Element {
+  return (
+    <main>
+      <div className={styles.posts}>
+        <a href="#null">
+          <h1>Como utilizar o Hooks</h1>
+          <p>Pensando em sincronização em vez de ciclos de vida.</p>
+          <div className={styles.postInfo}>
+            <div>
+              <FiCalendar />
+              15 Mar 2022
+            </div>
+            <div>
+              <FiUser />
+              Eleson Souza
+            </div>
+          </div>
+        </a>
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
+        <a href="#null">
+          <h1>Como utilizar o Hooks</h1>
+          <p>Pensando em sincronização em vez de ciclos de vida.</p>
+          <div className={styles.postInfo}>
+            <div>
+              <FiCalendar />
+              15 Mar 2022
+            </div>
+            <div>
+              <FiUser />
+              Eleson Souza
+            </div>
+          </div>
+        </a>
 
-//   // TODO
+        <a href="#null">
+          <h1>Como utilizar o Hooks</h1>
+          <p>Pensando em sincronização em vez de ciclos de vida.</p>
+          <div className={styles.postInfo}>
+            <div>
+              <FiCalendar />
+              15 Mar 2022
+            </div>
+            <div>
+              <FiUser />
+              Eleson Souza
+            </div>
+          </div>
+        </a>
+
+        <a href="#null">
+          <h1>Como utilizar o Hooks</h1>
+          <p>Pensando em sincronização em vez de ciclos de vida.</p>
+          <div className={styles.postInfo}>
+            <div>
+              <FiCalendar />
+              15 Mar 2022
+            </div>
+            <div>
+              <FiUser />
+              Eleson Souza
+            </div>
+          </div>
+        </a>
+
+        <button type="button">Carregar mais posts</button>
+      </div>
+    </main>
+  );
+}
+
+//  export const getStaticProps = async () => {
+//    /* const prismic = getPrismicClient({});
+//    const postsResponse = await prismic.getByType(TODO); */
+
+//    // TODO
 // };
